9c3fc77236aaf8a733fc7d53ec2f81540d484640
Hector Garcia <hmgarcia06@gmail.com>
2026-04-16 10:37:11 -0400

    added rewrites function to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // NOTE: Hides the Next.js on-screen dev indicator/devtools widget in development.
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/python-api/:path*",
+        destination: "http://localhost:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
