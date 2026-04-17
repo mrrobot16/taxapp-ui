16bd4937a16564750a89165e254aa0b797d73721
Hector Garcia <hmgarcia06@gmail.com>
2026-04-17 10:09:54 -0400

    Minor changes in layout title tag and in app/page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Tax App",
+  title: "Tax App - V1",
   description: "Tax App | The tax accountant’s sidekick",
   icons: {
     icon: "/favicon.ico",
