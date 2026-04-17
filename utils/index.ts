0c7dbd545a883db4b9e66064265504c56ac724b7
Hector Garcia <hmgarcia06@gmail.com>
2026-04-17 12:49:38 -0400

    Added utility that check if browser is mobile

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,7 @@ import { RANDOM_ID_SLICE_END, RANDOM_ID_SLICE_START } from "@/config";
 export function uid() {
   return Math.random().toString(36).slice(RANDOM_ID_SLICE_START, RANDOM_ID_SLICE_END);
 }
+
+export function isMobile() {
+  return typeof window !== "undefined" && window.matchMedia("(max-width: 767px)").matches;
+}
