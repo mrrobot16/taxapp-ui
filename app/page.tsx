95b648773272dc02cbd3d50f26d8078d980a5989
Hector Garcia <hmgarcia06@gmail.com>
2026-04-17 12:52:28 -0400

    Implement Chat component in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-const styles = {
-  container: "flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black",
-  main: "flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start",
-}
+import { Chat } from "@/components";
 
 export default async function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Hello World</h1>
-      </main>
-    </div>
+    <Chat />
   );
 }
